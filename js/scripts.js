e39055df8da0ba4014f7d9fc1a983a62b5b83a65
Abdulaziz9600 <erkinovch2000@gmail.com>
2022-05-23 18:25:20 +0500

    git

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 let elCinemaList = $(".js-cinema-list");
 let elFormSerach = $(".js-form-search");
 let elFlimSerach = $(".js-films-search");
+let elFilmCategories = $(".js-select-categories");
+let elFilmRating = $(".js-select-rating");
+let elFilmSort = $(".js-select-sort")
 let elMovieTemplate = $("#movies-template").content;
 
 movies.splice(100);
@@ -19,11 +22,12 @@ let normalizedMovies = movies.map((movie) => {
 })
 
 let createMovieElement = (movie) => {
+
   elCinemaList.innerHTML = "";
 
   let movieElement = elMovieTemplate.cloneNode(true);
 
-  $(".card-title", movieElement).textContent = movie.title.to;
+  $(".card-title", movieElement).textContent = movie.title.toString();
   $(".card-img-top", movieElement).src = movie.poster;
   $(".card-img-top", movieElement).alt = movie.title;
   $(".card-cast", movieElement).textContent = movie.fulltitle;
@@ -35,6 +39,7 @@ let createMovieElement = (movie) => {
 }
 
 let renderMovies = (movies) => {
+  elCinemaList.innerHTML = "";
   let elResultFragment = document.createDocumentFragment();
 
   movies.forEach((movie) => {
@@ -46,7 +51,7 @@ let renderMovies = (movies) => {
 
 renderMovies(normalizedMovies);
 
-elFormSerach.addEventListener("submit", (evt) => {
+elFlimSerach.addEventListener("change", (evt) => {
   evt.preventDefault();
 
   let searchMovie = new RegExp(elFlimSerach.value.trim(), "gi");
@@ -57,4 +62,88 @@ elFormSerach.addEventListener("submit", (evt) => {
     }
   })
   renderMovies(searchResult);
+})
+
+
+let numberCategories = [];
+
+normalizedMovies.forEach((film) => {
+  film.categories.split(", ").forEach((categori) => {
+    if(!numberCategories.includes(categori)){
+      numberCategories.push(categori);
+    }
+  })
+})
+
+numberCategories.forEach((movi)=>{
+
+  let newOption = createElement("option","","");
+  newOption.textContent = movi;
+  newOption.option = movi;
+  elFilmCategories.append(newOption);
+
+  
+})
+  
+elFilmCategories.addEventListener("change", (evt)=>{
+  evt.preventDefault();
+  elCinemaList.innerHTML = "";
+
+  let categoryMovies = normalizedMovies.filter(function(movie) {
+    if (movie.categories.includes(elFilmCategories.value)){
+      return movie.categories.includes(elFilmCategories.value)
+    }
+  })
+
+  renderMovies(categoryMovies)
+})
+
+elFilmRating.addEventListener("change", (evt) => {
+  evt.preventDefault();
+  let newRating = [];
+
+  normalizedMovies.forEach((kino) =>{
+
+if(kino.imdbRating >= elFilmRating.value){
+   newRating.push(kino);
+}
+  })
+  renderMovies(newRating);
+})
+
+let newOptionArry = ["A-Z","Z-A","Reating=>","Reating<="];
+
+
+newOptionArry.forEach((grow)=>{
+  
+  let newOption = createElement("option","",grow);
+  newOption.textContent = grow;
+  newOption.value = grow;
+  
+  elFilmSort.append(newOption);
+}) 
+
+let sortMovies = []
+if (elFilmSort.value == newOptionArry[0]){
+  sortMovies = normalizedMovies.sort((a,b)=> a.title.localeCompare(b.title))
+}
+
+elFilmSort.addEventListener("change", (evt)=>{
+  evt.preventDefault();
+  elCinemaList.innerHTML = "";
+  let sortMovies = [];
+    if (elFilmSort.value == newOptionArry[0]){
+      sortMovies = normalizedMovies.sort((a,b)=> a.title.localeCompare(b.title))
+    }
+    if (elFilmSort.value == newOptionArry[1]){
+      sortMovies = normalizedMovies.sort((a,b)=> b.title.localeCompare(a.title))
+    }
+    if (elFilmSort.value == newOptionArry[2]){
+      sortMovies = normalizedMovies.sort((a,b)=> b.raiting - a.raiting)
+    }
+    if (elFilmSort.value == newOptionArry[3]){
+      sortMovies = normalizedMovies.sort((a,b)=> a.raiting - b.raiting)
+    }
+   
+  renderMovies(sortMovies)
 })
